ee867cde3c209c409974882c788c18df58c214f2
rusteex11Js <vinothjsautomation@gmail.com>
2025-03-14 14:02:02 +0530

    added the api service

--- test/pact/services/api-services.ts
+++ test/pact/services/api-services.ts
@@ -6,7 +6,7 @@ export class ApiService {
     private url: string;
     private port: number;
 
-    constructor(endpoint: { url: string; port: number }) {
+    constructor(endpoint: { url: string, port: number }) {
         this.url = endpoint.url;
         this.port = endpoint.port;
     }
@@ -22,4 +22,38 @@ export class ApiService {
         });
     };
 
+    public create = (endpointUrl: string, data:JSON, headers?: AxiosHeaders, query?: any): AxiosPromise => {
+        return axios.request({
+            baseURL: `${this.url}:${this.port}`,
+            method: 'POST',
+            headers,
+            url: endpointUrl,
+            data,
+            httpAgent: new http.Agent({ keepAlive: false }),
+            params: query,
+        });
+    };
+
+    public update = (endpointUrl: string, data:JSON, headers?: AxiosHeaders, query?: any): AxiosPromise => {
+        return axios.request({
+            baseURL: `${this.url}:${this.port}`,
+            method: 'PUT',
+            headers,
+            url: endpointUrl,
+            data,
+            httpAgent: new http.Agent({ keepAlive: false }),
+            params: query,
+        });
+    };
+
+    public delete = (endpointUrl: string, headers?: AxiosHeaders, query?: any): AxiosPromise => {
+        return axios.request({
+            baseURL: `${this.url}:${this.port}`,
+            method: 'DELETE',
+            headers,
+            url: endpointUrl,
+            httpAgent: new http.Agent({ keepAlive: false }),
+            params: query,
+        });
+    };
 }
